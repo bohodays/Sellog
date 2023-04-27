5fd67412f45a61397b4e897f51e96e18369efba1
unknown <pjw2369@naver.com>
2023-04-27 23:34:56 +0900

    design : UI 작업

--- front-end/src/components/MyGoals/GoalsSetting/GoalsSetting.tsx
+++ front-end/src/components/MyGoals/GoalsSetting/GoalsSetting.tsx
@@ -1,14 +1,28 @@
 import React from "react";
 import { SSection } from "./styles";
+import RetroPencil from "@/assets/imgs/retro/pencil_retro.png";
+import WorkHard from "@/assets/imgs/retro/work_hard.png";
+import SettingButtons from "../SettingButtons/SettingButtons";
+import Spring from "@/assets/imgs/retro/spring.png";
 
 const GoalsSetting = () => {
   return (
     <SSection>
       <div className="title__wrapper">
-        <h1 className="title">MY GOALS</h1>
+        <p className="title">MY GOALS</p>
         <span>kepp running to achieve your goal !</span>
+        <img
+          className="sticker retro__pencil"
+          src={RetroPencil}
+          alt="retro_pencil"
+        />
+        <img className="sticker retro__work" src={WorkHard} alt="work_hard" />
       </div>
-      <div className="content__wrapper">d</div>
+      <div className="content__wrapper">
+        <img className="sticker retro__spring" src={Spring} alt="spring" />
+        {/* 목표 설정 버튼들 */}
+        <SettingButtons />
+      </div>
     </SSection>
   );
 };
