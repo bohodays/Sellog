283d3bf606c14802ff2e75f1b49276b572571f24
leo9876 <leo9876@naver.com>
2023-05-01 16:59:58 +0900

    feat : myroom api recoil 예시코드

--- front-end/src/recoil/myroom/atoms.tsx
+++ front-end/src/recoil/myroom/atoms.tsx
@@ -1,6 +1,41 @@
-import { atom } from "recoil";
+import { userInfo } from "os";
+import { atom, useRecoilState } from "recoil";
 
-const myNewAtom = atom({
+type userInfo = {
+  userId: number;
+  nickname: string;
+  img: string;
+  email: string;
+  bojTarget: string;
+  blogTarget: string;
+  feedTarget: string;
+  csTarget: string;
+  velog: string;
+  tistory: string;
+  github: string;
+  motto: string;
+  characterId: number;
+  tel: string;
+  contact: string;
+};
+
+export const user = atom<userInfo>({
   key: "myNewAtom",
-  default: value,
+  default: {
+    userId: 0,
+    nickname: "",
+    img: "",
+    email: "",
+    bojTarget: "",
+    blogTarget: "",
+    feedTarget: "",
+    csTarget: "",
+    velog: "",
+    tistory: "",
+    github: "",
+    motto: "",
+    characterId: 0,
+    tel: "",
+    contact: "",
+  },
 });
