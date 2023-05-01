60d468025ffc7d4996f63c2c745fecea8af1a967
jisoo <dlwltn0350@naver.com>
2023-05-01 17:51:18 +0900

    update : manifest.json 설정

--- chrome-extension/scripts/tistory/tistory.js
+++ chrome-extension/scripts/tistory/tistory.js
@@ -4,25 +4,29 @@
 let loader;
 
 const currentUrl = window.location.href;
+console.log(currentUrl);
 
-// 버튼 누르면 제출
 if (currentUrl.includes('tistory.com/manage/newpost')) {
-    document.querySelector("#publish-btn").addEventListener('click', async () => {
-        try{
-            const title = document.querySelector("#post-title-inp").textContent;
-            const content = document.querySelector("#tinymce").textContent;
-            const url = document.querySelector("#editor-root > div:nth-child(42) > div > div > div > form > fieldset > div.layer_body > div > dl:nth-child(5) > dd > span").textContent
-                + title;
-            const message = `[Tistory] Title: ${title}, content: ${content}`+'\n\n'+
-                    `URL: ${url}`;
-            
-            console.log(message);
-            await uploadOnePostingOnSellog(message);
-            
-        } catch(error){
-            console.log(error);
-        }
+  window.addEventListener('DOMContentLoaded', () => {
+    const publishBtn = document.querySelector("#publish-btn");
+    if (!publishBtn) return;
+    console.log(publishBtn);
+
+    publishBtn.addEventListener('click', async () => {
+      console.log("1111");
+      try {
+        const title = document.querySelector("#post-title-inp").textContent;
+        const content = document.querySelector("#tinymce").textContent;
+        const url = document.querySelector("#editor-root > div > div > div > div > form > fieldset > div.layer_body > div > dl:nth-child(5) > dd > span").textContent + title;
+        const message = `[Tistory] Title: ${title}, content: ${content}`+'\n\n'+ `URL: ${url}`;
+                
+        console.log(message);
+        await uploadOnePostingOnSellog(message);
+      } catch(error){
+        console.log(error);
+      }
     })
+  });
 }
 
   
