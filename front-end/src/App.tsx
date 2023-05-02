ec2e7474ea07cd43d65b1bb246716902fae24e0b
unknown <pjw2369@naver.com>
2023-05-02 10:07:48 +0900

    chore : readme 수정

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,6 +1,4 @@
 import React, { Suspense, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { RecoilRoot } from "recoil";
