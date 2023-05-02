c08ead27cd4212e82c53b99144a86f83afcb82e8
찬휘 <cksgnlcjswoo@naver.com>
2023-05-02 20:23:38 +0900

    Feat: feed 받아오 기능

--- back-schedule/src/main/java/com/example/estable/dto/FeedDto.java
+++ back-schedule/src/main/java/com/example/estable/dto/FeedDto.java
@@ -1,6 +1,9 @@
 package com.example.estable.dto;
 
+import com.example.estable.entity.Feed;
 import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -9,8 +12,17 @@ import lombok.*;
 @AllArgsConstructor
 public class FeedDto {
 
-    private String url;
-    private String description;
+    private String link;
     private String title;
     private String company;
+    private LocalDateTime pub_date;
+
+    public Feed toEntity() {
+        return Feed.builder()
+                .link(link)
+                .pub_date(pub_date)
+                .company(company)
+                .title(title)
+                .build();
+    }
 }
--- back-schedule/src/main/java/com/example/estable/schedule/feed/FeedService.java
+++ back-schedule/src/main/java/com/example/estable/schedule/feed/FeedService.java
@@ -1,10 +1,14 @@
 package com.example.estable.schedule.feed;
 
+import com.example.estable.dto.FeedDto;
+import com.example.estable.entity.Feed;
+import com.example.estable.repository.FeedRepository;
 import com.sun.syndication.feed.synd.SyndContent;
 import com.sun.syndication.feed.synd.SyndEntry;
 import com.sun.syndication.feed.synd.SyndFeed;
 import com.sun.syndication.io.SyndFeedInput;
 import com.sun.syndication.io.XmlReader;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -18,13 +22,17 @@ import java.io.InputStream;
 import java.io.StringReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.time.ZoneId;
 import java.util.Date;
 import java.util.List;
 
 
 @Slf4j
+@RequiredArgsConstructor
 @Service
 public class FeedService {
+
+    private final FeedRepository feedRepository;
 
     private String[] company = {"MUSINSA","NHN Toast","당근","토스"};
     private String[] urls = {
@@ -53,13 +61,28 @@ public class FeedService {
             for(SyndEntry entry : entries) {
 
                 String title = entry.getTitle();
+
+                Feed result = feedRepository.findByTitle(title);
+
+                //최신순이므로 이미 있다면 제외
+                if(result != null) break;
+
                 Date publishedDate = entry.getPublishedDate();
                 String link = entry.getLink();
 
                 //DB에 넣기
                 log.info("title {}",title);
-                log.info("published Date {}",publishedDate);
+                log.info("published Date {}",publishedDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime());
                 log.info("link {}",link);
+
+                FeedDto feedDto = new FeedDto().builder()
+                        .title(title)
+                        .link(link)
+                        .company(company[i])
+                        .pub_date(publishedDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime())
+                        .build();
+
+                feedRepository.save(feedDto.toEntity());
             }
 
 
