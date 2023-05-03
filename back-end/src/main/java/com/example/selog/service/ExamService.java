0ea2f115bdfc16b31f848c481c53f1b6859b4fe7
jisoo <dlwltn0350@naver.com>
2023-05-03 15:38:58 +0900

    update : transaction

--- back-end/src/main/java/com/example/selog/service/ExamService.java
+++ back-end/src/main/java/com/example/selog/service/ExamService.java
@@ -6,6 +6,7 @@ import com.example.selog.repository.ExamRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,6 +16,7 @@ public class ExamService {
 
     private final ExamRepository examRepository;
 
+    @Transactional
     public List<ExamDto> getExamList() {
 
         long start = (long)(Math.random() * 200);
