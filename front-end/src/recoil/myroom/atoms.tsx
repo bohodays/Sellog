137a73ee56ef73ca710aa46674a74ecb3ea0fc83
unknown <pjw2369@naver.com>
2023-05-03 13:59:11 +0900

    update : 유저 정보 api 요청

--- front-end/src/recoil/myroom/atoms.tsx
+++ front-end/src/recoil/myroom/atoms.tsx
@@ -2,7 +2,7 @@ import { userInfo } from "os";
 import { atom, useRecoilState } from "recoil";
 
 type userInfo = {
-  userId: number;
+  userId: number | null;
   nickname: string;
   img: string;
   email: string;
@@ -14,7 +14,7 @@ type userInfo = {
   tistory: string;
   github: string;
   motto: string;
-  characterId: number;
+  characterId: number | null;
   tel: string;
   contact: string;
 };
@@ -22,20 +22,20 @@ type userInfo = {
 export const userInfoState = atom<userInfo>({
   key: "UserInfo",
   default: {
-    userId: 1,
-    nickname: "Mojung",
+    userId: null,
+    nickname: "",
     img: "",
-    email: "hohohojung@gmail.com",
-    bojTarget: "1 commit per day",
-    blogTarget: "1 post per week",
-    feedTarget: "1 issue per day",
-    csTarget: "5 quiz per day",
-    velog: "hojung@velog.io",
-    tistory: "https://hojung@tistory.com",
-    github: "https://github.com/Dohyun-Kimm",
-    motto: "You are made of your past",
-    characterId: 2,
-    tel: "010-1234-5678",
-    contact: "hojung@gmail.com",
+    email: "",
+    bojTarget: "",
+    blogTarget: "",
+    feedTarget: "",
+    csTarget: "",
+    velog: "",
+    tistory: "",
+    github: "",
+    motto: "",
+    characterId: null,
+    tel: "",
+    contact: "",
   },
 });
