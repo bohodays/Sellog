6a0b49210c1501c7d3dc87787d2ef5303dd71dfa
hojung <hoguangel@gmail.com>
2023-05-04 17:53:16 +0900

    feat : myrecords api 연결 ing

--- front-end/src/components/MyGoals/DailyDoneItem/DailyDoneItem.tsx
+++ front-end/src/components/MyGoals/DailyDoneItem/DailyDoneItem.tsx
@@ -1,14 +1,18 @@
 import { SDiv } from "./styles";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSquareCheck } from "@fortawesome/free-regular-svg-icons";
+import { ICountDoneItem } from "@/typeModels/mygoals/myRecordInterface";
 
-const DailyDoneItem = ({ doneItem }: any) => {
+interface ICountDoneProps {
+  doneItem: ICountDoneItem;
+}
+const DailyDoneItem = ({ doneItem }: ICountDoneProps) => {
   return (
     <SDiv>
       <div className="doneItem__wrapper">
         <FontAwesomeIcon icon={faSquareCheck} />
         <div>{doneItem?.type}</div>
-        <div>{doneItem?.doneCount}</div>
+        <div>{doneItem?.count}</div>
       </div>
     </SDiv>
   );
--- front-end/src/components/MyGoals/DailyDoneList/DailyDoneList.tsx
+++ front-end/src/components/MyGoals/DailyDoneList/DailyDoneList.tsx
@@ -1,14 +1,18 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { SSection } from "./styles";
 import cool from "@/assets/imgs/itemShop/cool.png";
 import rainbow from "@/assets/imgs/itemShop/rainbow.png";
 import DailyDoneItem from "../DailyDoneItem/DailyDoneItem";
 import { apiGetMonthlyRecordList, apiGetTodayRecord } from "@/api/record";
+import {
+  IDoneList,
+  ICountDoneItem,
+} from "@/typeModels/mygoals/myRecordInterface";
 
 interface ISelectedDate {
   selectedDateProps: Date;
 }
-const dummyDoneList = [
+const countDone = [
   { type: "github", doneCount: 3 },
   { type: "blog", doneCount: 1 },
   { type: "csquiz", doneCount: 4 },
@@ -16,21 +20,45 @@ const dummyDoneList = [
   { type: "feed", doneCount: 2 },
 ];
 const DailyDoneList = ({ selectedDateProps }: ISelectedDate) => {
-  // apiGetMonthlyRecordList(2023, 5).then((r) => {
-  //   console.log(r);
-  // });
-  // apiGetTodayRecord().then((r) => {
-  //   console.log("오늘", r);
-  // });
+  const [doneList, setDoneList] = useState<IDoneList | null>(null);
+  const [todayDoneList, setTodayDoneList] = useState<IDoneList | null>(null);
+  const [countDoneList, setCountDoneList] = useState<ICountDoneItem[] | null>(
+    []
+  );
+
+  useEffect(() => {
+    apiGetTodayRecord().then((r) => {
+      setTodayDoneList(() => r?.data.response);
+      const newTodayDoneList = [];
+      for (let item in r?.data.response) {
+        newTodayDoneList.push({
+          type: item,
+          count: r?.data.response[item]?.length || 0,
+        });
+      }
+      setCountDoneList(newTodayDoneList);
+    });
+  }, []);
+
+  useEffect(() => {
+    apiGetMonthlyRecordList(2023, 5).then((r) => {
+      setDoneList(r?.data.response);
+      console.log("롸", doneList);
+    });
+  }, []);
+
   return (
     <SSection>
       <div className="selected__date">
         {selectedDateProps.toLocaleDateString()}
       </div>
       <img className="cool__img" src={cool}></img>
-      {dummyDoneList.map((item, index) => (
-        <DailyDoneItem doneItem={item} key={index}></DailyDoneItem>
-      ))}
+      {
+        // countDoneList != null &&
+        countDoneList?.map((item, index) => (
+          <DailyDoneItem doneItem={item} key={index}></DailyDoneItem>
+        ))
+      }
       <img className="rainbow__img" src={rainbow}></img>
     </SSection>
   );
