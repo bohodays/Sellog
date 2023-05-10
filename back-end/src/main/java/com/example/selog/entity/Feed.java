076f36b0825fc851c354f990e42c3cb1df498998
jisoo <dlwltn0350@naver.com>
2023-05-10 15:54:07 +0900

    update : @DynamicInsert 추가

--- back-end/src/main/java/com/example/selog/entity/Feed.java
+++ back-end/src/main/java/com/example/selog/entity/Feed.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
 import org.springframework.boot.context.properties.bind.DefaultValue;
 
 import javax.persistence.*;
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@DynamicInsert
 @Getter
 @Builder
 @Table(name = "feed")
